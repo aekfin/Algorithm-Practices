573a188de36e70b871c86dad51dae55b02c4d867
ThELaval <thelaval.lol@gmail.com>
2018-03-10 15:02:26 +0700

    Success MergeKSortLists

--- MergeKSortedLists.js
+++ MergeKSortedLists.js
@@ -17,42 +17,45 @@ class Node {
   }
 }
 
-function MergeKSortedLists (listA, listB) {
-  var a = listA
-  var b = listB
+function MergeKSortedLists (lists) {
   var node = null
   var head = null
-  while (a && b) {
-    var less = null
-    if (a.data < b.data) {
-      less = a.data
-      a = a.next
-    } else {
-      less = b.data
-      b = b.next
+  while (lists.length > 0){
+    var less = {
+      value: 10000,
+      node: null
     }
-    if (node) {
-      node.next = new Node(less, null)
-      node = node.next
-    } else {
-      node = new Node(less, null)
-      head = node
+    for (var i = 0; i < lists.length; i++) {
+      if (less.value > lists[i].data) {
+        less.value = lists[i].data
+        less.index = i
+      }
     }
-  }
-  if (a) {
-    var left = a
-  } else {
-    var left = b
-  }
-  while (left) {
-    node.next = new Node(left.data, null)
-    node = node.next
-    left = left.next 
+    if (lists.length > 0 && less.value !== 10000) {
+      if (node) {
+        node.next = new Node(less.value, null)
+        node = node.next
+      } else {
+        node = new Node(less.value, null)
+        head = node
+      }
+    }
+    if (lists[less.index]) {
+      lists[less.index] = lists[less.index].next
+    }
+    for (var i = 0; i < lists.length; i++) {
+      if (!lists[i]) {
+        lists.splice(i, 1)
+      }
+    }
   }
 
   return head
 }
 
-var listA = new Node(1, new Node(7, new Node(8, null)))
+var listA = new Node(1, new Node(7, new Node(50, null)))
 var listB = new Node(2, new Node(5, new Node(6, null)))
-MergeKSortedLists(listA, listB).print()
+var listC = new Node(3, new Node(10, new Node(100, null)))
+var listD = new Node(4, new Node(20, new Node(200, null)))
+var list = [listA, listB, listC, listD]
+MergeKSortedLists(list).print()
